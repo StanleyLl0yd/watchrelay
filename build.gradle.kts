78f09dff980ce7c2157282f4f3ee50bbef804ab4
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-01 21:51:08 +0200

    build: register Room schema export plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.androidx.room3) apply false
 }
