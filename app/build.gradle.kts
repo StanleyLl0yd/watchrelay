2575b2c594ef932a37afd1c35c1c3e7973b6e833
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-01 18:59:06 +0200

    release: set version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.sl.watchrelay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1-dev"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     signingConfigs {
